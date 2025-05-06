837a70c367b630f6e14c9b66f58557fcc6a7594c
Katlin Johnston <300077526@fvtc.edu>
2025-05-06 15:18:20 -0500

    working on admin form

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.static('public'))
 
 
-app.get('/api/v1/menu/:id',async (request, response)=>{
+app.get('/api/v1/menu/:id',async (request, response) =>{
     const menu = await getCollection('Menu','Menu')
     const {id} = request.params
     const found = await menu.findOne({"_id": new ObjectId(id) })
@@ -18,7 +18,7 @@ app.get('/api/v1/menu/:id',async (request, response)=>{
     else response.send('nothing has found') 
 })
 
-app.get('/api/v1/menu', async(request, response)=>{
+app.get('/api/v1/menu', async(request, response) =>{
     const menu = await getCollection('Menu','Menu' )
     const found = await menu.find({}).toArray()
     if (found) response.send(found)
@@ -26,7 +26,7 @@ app.get('/api/v1/menu', async(request, response)=>{
 })
 
 
-app.get('/api/v1/events',async(request, response)=>{
+app.get('/api/v1/events',async(request, response) =>{
         const event = await getCollection('Events','Info')
         const found = await event.find({}).toArray()
         if (found) response.send(found)
@@ -34,7 +34,7 @@ app.get('/api/v1/events',async(request, response)=>{
     
 })
 
-app.get('/api/v1/events/:id',async(request, response)=>{
+app.get('/api/v1/events/:id',async(request, response) =>{
     const event = await getCollection('Events','Info')
     const {id} = request.params
     const found = await event.findOne({"_id": new ObjectId(id) })
@@ -43,34 +43,34 @@ app.get('/api/v1/events/:id',async(request, response)=>{
 })
 
 
-app.post('/api/v1/menu', async(request,response)=>{
+app.post('/api/v1/menu', async(request,response) =>{
     const {name, description, price, img } = request.body
 
     if(!name || !description || !price || !img){
-        response.status(400).send({error: true, message: `missing information`})
+        response.status(400).send({error: true, message: `Missing information`})
         return
     }   
     const collection = await getCollection('Menu', 'Menu')    
     const found = await collection.findOne({"name": 'name'})
 
-    if (found) return response.send(400).send({error: true, message: `already exist`})
+    if (found) return response.send(400).send({error: true, message: `Already exist`})
     const {acknowledged, insertedID} = await collection.insertOne({name, description, price, img})
     response.send({ acknowledged, insertedID})
     response.send("working on this")
 })
 
 
-app.post('/api/v1/events',async(request,response)=>{
+app.post('/api/v1/events',async(request,response) =>{
     const {Name, Location, Date, Time } = request.body
 
     if(!Name || !Location || !Date || !Time){
-        response.status(400).send({error: true, message: `missing information`})
+        response.status(400).send({error: true, message: `Missing information`})
         return
     }   
     const collection = await getCollection('Events', 'Info')    
     const found = await collection.findOne({"name": 'name'})
 
-    if (found) return response.send(400).send({error: true, message: `already exist`})
+    if (found) return response.send(400).send({error: true, message: `Already exist`})
     const {acknowledged, insertedID} = await collection.insertOne({Name, Location, Date, Time})
     response.send({ acknowledged, insertedID})
     response.send("working on this")
